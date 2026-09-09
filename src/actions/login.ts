1fee54de2a308d507cf3af05663e43bd46e4b1f0
fazle-rabbi-dev <frabby983@gmail.com>
2026-09-09 23:59:56 +0600

    feat(auth): add login and logout server action

---
+++ src/actions/login.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import setCookieParser from 'set-cookie-parser';
+import { loginUser } from '@/lib/api/auth';
+import { LoginFormData } from '@/validations/auth.validation';
+
+const loginUserAction = async (formData: LoginFormData) => {
+   const { response, body } = await loginUser(formData);
+   const cookieStore = await cookies();
+
+   const rawCookies = response?.headers.getSetCookie?.() ?? [];
+   const parsedCookies = setCookieParser.parse(rawCookies, { map: false });
+   console.log(parsedCookies);
+
+   for (const c of parsedCookies) {
+      cookieStore.set(c.name, c.value, {
+         httpOnly: c.httpOnly,
+         secure: c.secure,
+         path: c.path,
+         maxAge: c.maxAge,
+         sameSite: c.sameSite as 'strict' | 'lax' | 'none',
+         expires: c.expires,
+      });
+   }
+
+   return body;
+};
+
+export default loginUserAction;
